d48e13416f6e4e1f8b3752bcdbdcadf0d31b9cc8
cannoli9917 <katie.pennachio@gmail.com>
2019-03-19 17:27:18 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,13 +3,17 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  renderPortfolio = () => {
+    return this.props.portfolio.map(port => {
+      return <Stock stock={port} handleClick={this.props.handleClick}/>
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+        {this.renderPortfolio()}
       </div>
     );
   }
