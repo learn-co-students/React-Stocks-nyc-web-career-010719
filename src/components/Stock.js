5810a73bc3804c3566bc20a2b939ffeb69856e34
purplesands <mullenjoe13@gmail.com>
2019-03-20 09:06:46 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,25 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
+const Stock = (props) => {
 
-    <div className="card">
+const addStockLocal = () => {
+  props.addStock(props.id)
+  }
+
+return(
+  <div>
+    <div className="card" onClick={addStockLocal}>
       <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
+        <h5 className="card-title" >{
+            props.name
           }</h5>
         <p className="card-text">{
-            //ticker: stock price
+            props.price
           }</p>
       </div>
     </div>
-
-
   </div>
-);
+  )
+};
 
 export default Stock
