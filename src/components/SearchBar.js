353ad289f7da38a33bd505fa5775ba4f13ec5bd7
github username <clarencekwong7@gmail.com>
2019-03-20 15:12:23 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,29 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ handleSort }) => {
+
+  const handleChange = (e) => {
+    // debugger
+    handleSort(e.target.value)
+  }
+
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={null} onChange={handleChange}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={null} onChange={handleChange}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleChange}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,25 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
+const Stock = ({ stock, buyStock, sellStock }) => {
 
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+  const handleClick = () => {
+    if (buyStock) {
+      return buyStock(stock)
+    } else {
+      return sellStock(stock)
+    }
+  }
+
+  return(
+    <div>
+      <div className="card" onClick={handleClick}>
+        <div className="card-body">
+          <h5 className="card-title">{stock.name}</h5>
+          <p className="card-text">{stock.price}</p>
+        </div>
       </div>
     </div>
-
-
-  </div>
-);
+  )
+};
 
 export default Stock
